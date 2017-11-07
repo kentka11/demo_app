22a3d3b6a5dbcb658e4e8c233d38d28ceb16cd9b
vagrant <vagrant@localhost.localdomain>
2017-11-07 09:44:19 +0000

    first commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,11 @@
+module ApplicationHelper
+	# ページごとの完全なタイトルを返します。
+	def full_title(pae_title='')
+		base_title = "Ruyb on Rails Tutorial Sample App"
+		if base_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
+end
