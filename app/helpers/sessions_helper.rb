06dbf372fbc367ab3bb93939b24ae5da128f090e
root <root@localhost.localdomain>
2017-11-11 10:22:14 +0000

    9

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,22 +1,46 @@
-module SessionsHelper
+	module SessionsHelper
 
   # 渡されたユーザーでログインする
   def log_in(user)
   	session[:user_id] = user.id
   end
 
-  # 現在ログイン中のユーザを返す(存在する場合)
+  # ユーザのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # 記憶トークンcookieに対応するユーザを返す
   def current_user
-  	# Rubyの||=というイディオムは左辺が未定義または偽なら代入の意味
-  	@current_user ||= User.find_by(id: session[:user_id])
+  	if (user_id = session[:user_id]) 
+  		# Rubyの||=というイディオムは左辺が未定義または偽なら代入の意味
+  		@current_user ||= User.find_by(id: session[:user_id])
+  	elsif (user_id = cookies.signed[:user_id])
+  		user = User.find_by(id: user.id)
+  		if user && user.authenticated?(cookies[:remember_token])
+  			log_in user
+  			@current_user = user
+  		end
+  	end
   end
 
   def logged_in?
   	!current_user.nil?
   end
 
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # 現在のユーザーをログアウトする
   def log_out
-  	session.delete(:user_id)
-  	@current_user = nil
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
