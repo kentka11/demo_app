cb8da8315ff020b53f3781a90a65eaf28760d0d9
vagrant <vagrant@localhost.localdomain>
2017-11-13 12:05:37 +0000

    doing 11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,8 +19,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-      log_in @user
-  		flash[:success] = "Welcome to the Sample App!"
+      UserMailer.account_activation(@user_params)
+  		flash[:success] = "Please check your email to activate your account."
   		redirect_to @user
   	else
   		render 'new'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name:  "Example User",
+User.create!(name: "Example User",
              email: "example@railstutorial.org",
-             password:              "Welcome1",
-             password_confirmation: "Welcome1",
-             admin: true)
+             passowrd: "Welcome1"
+             passowrd_confirmation: "Welcome1",
+             admin: true,
+             activated: true
+             activated_at: Time.zone.now)
 
 99.times do |n|
-  name  = Faker::Name.name
+  name = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+  password = "Welcome1"
+  User.create!(password: password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
