055554292f687bbcf3dfdc56ebc40755b39e30bf
vagrant <vagrant@localhost.localdomain>
2017-11-13 06:36:59 +0000

    10 done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   # 編集処理が行われる前にログインを要求する処理
-  before_action :logged_in_user, only: [:index, :edit, :update, :show]
-  before_action :correct_user, only: [:edit, :update, :show]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destory]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   def show
@@ -40,6 +41,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
     def user_params
     	params.require(:user).permit(:name, :email, :password, :password_confirmation)
@@ -58,5 +65,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless @user == current_user
+    end
+
+    #管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
